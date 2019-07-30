808855a90545927ae5d1912d5130268dba3e4567
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-30 11:44:22 +1000

    Create xfce_setup.sh

---
+++ xfce_setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -x
+
+set -e
+set -u
+
+sh cores.sh
+#xdg-user-dirs-update
+
+sudo pacman -Syuu --noconfirm --needed
+sudo pacman -S --noconfirm --needed arch-install-scripts
+#sudo pacman -S --noconfirm --needed xorg
+sudo pacman -S --noconfirm --needed openssh
+sudo systemctl enable sshd
+sudo pacman -S --noconfirm --needed dhcpcd
+
+sudo pacman -S xfce4 xfce4-goodies --noconfirm --needed
+
+sudo pacman -S pulseaudio --noconfirm --needed
+sudo pacman -S pulseaudio-alsa --noconfirm --needed
+sudo pacman -S pavucontrol  --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-lib alsa-firmware --noconfirm --needed
+sudo pacman -S gstreamer --noconfirm --needed
+sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly --noconfirm --needed
+
+
+sudo pacman -S --noconfirm --needed grub-customizer 
+sudo pacman -S --noconfirm --needed wget
+sudo pacman -S --noconfirm --needed net-tools
+sudo pacman -S --noconfirm --needed htop
+sudo pacman -S --noconfirm --needed gtop
+sudo pacman -S --noconfirm --needed gparted
+sudo pacman -S --noconfirm --needed simplescreenrecorder
+sudo pacman -S --noconfirm --needed filezilla
+sudo pacman -S --noconfirm --needed atom
+sudo pacman -S --noconfirm --needed geany
+sudo pacman -S --noconfirm --needed meld
+sudo pacman -S --noconfirm --needed catfish
+sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller
+sudo pacman -S --noconfirm --needed firefox-developer-edition
+sudo pacman -S --noconfirm --needed qbittorrent
+sudo pacman -S --noconfirm --needed neofetch
+sudo pacman -S --noconfirm --needed chromium
+sudo pacman -S --noconfirm --needed libreoffice-fresh
+sudo pacman -S --noconfirm --needed mpv
+
+#sh sddm.sh
+
+sh samba_install.sh
+
+sh trizen.sh
+
+trizen -S --noconfirm --needed --noedit pamac-aur
+trizen -S --noconfirm --needed --noedit vivaldi
+trizen -S --noconfirm --needed --noedit vivaldi-codecs-ffmpeg-extra-bin
+trizen -S --noconfirm --needed --noedit realvnc-vnc-server
+trizen -S --noconfirm --needed --noedit realvnc-vnc-viewer
+#trizen -S --noconfirm --needed --noedit vlc-nightly
+trizen -S --noconfirm --needed --noedit inxi-git
+
+sudo /usr/bin/vnclicense -add QXHZK-RNRVT-TE3RG-ARWFF-V7WQA
+
+sudo systemctl enable vncserver-x11-serviced
+
+sudo systemctl enable NetworkManager
+
+
