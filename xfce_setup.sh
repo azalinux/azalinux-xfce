b6c19575cd31bdb2ee6dc73ed43fc5538f05038d
azalinux <52433182+azalinux@users.noreply.github.com>
2019-07-30 12:03:24 +1000

    Update xfce_setup.sh

--- xfce_setup.sh
+++ xfce_setup.sh
@@ -16,6 +16,20 @@ sudo pacman -S --noconfirm --needed dhcpcd
 sudo pacman -S xfce4 xfce4-goodies --noconfirm --needed
 sudo pacman -S --noconfirm --needed xfce4-notifyd
 
+
+sudo pacman -S adobe-source-sans-pro-fonts --noconfirm --needed
+sudo pacman -S cantarell-fonts --noconfirm --needed
+sudo pacman -S noto-fonts --noconfirm --needed
+sudo pacman -S terminus-font --noconfirm --needed
+sudo pacman -S ttf-bitstream-vera --noconfirm --needed
+sudo pacman -S ttf-dejavu --noconfirm --needed
+sudo pacman -S ttf-droid --noconfirm --needed
+sudo pacman -S ttf-inconsolata --noconfirm --needed
+sudo pacman -S ttf-liberation --noconfirm --needed
+sudo pacman -S ttf-roboto --noconfirm --needed
+sudo pacman -S ttf-ubuntu-font-family --noconfirm --needed
+sudo pacman -S tamsyn-font --noconfirm --needed
+
 sudo pacman -S pulseaudio --noconfirm --needed
 sudo pacman -S pulseaudio-alsa --noconfirm --needed
 sudo pacman -S pavucontrol  --noconfirm --needed
