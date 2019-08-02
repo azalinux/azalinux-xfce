d716499f3d6c4d274688830e6c4737cc301741a5
azalinux <52433182+azalinux@users.noreply.github.com>
2019-08-02 10:27:10 +1000

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-wget https://github.com/azalinux/azalinux/blob/master/theme/wallpaper/* -O /usr/share/backgrounds/xfce/
+sudo wget -i wallpaper.txt -O /usr/share/backgrounds/xfce/
 
 #sudo cp * /usr/share/backgrounds/xfce
