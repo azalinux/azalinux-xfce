7eb4f66a8dd1b358dbce701d4c8142c41f389b82
azalinux <52433182+azalinux@users.noreply.github.com>
2019-08-17 09:55:43 +1000

    Update lightdm.sh

--- lightdm.sh
+++ lightdm.sh
@@ -5,7 +5,7 @@ set -e
 sudo groupadd -r autologin
 sudo gpasswd -a aza autologin
 
-wget https://raw.githubusercontent.com/azalinux/azalinux-xfce/master/lightdm.conf -O /etc/lightdm/lightdm.conf
+sudo wget https://raw.githubusercontent.com/azalinux/azalinux-xfce/master/lightdm.conf -O /etc/lightdm/lightdm.conf
 
 sudo systemctl enable lightdm.service
 sudo systemctl set-default graphical.target
