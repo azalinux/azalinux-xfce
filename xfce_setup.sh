55580e9a7cd779d2ef7e5c6870a972454ca3a6bf
azalinux <52433182+azalinux@users.noreply.github.com>
2019-10-20 09:37:10 +1000

    Update xfce_setup.sh

--- xfce_setup.sh
+++ xfce_setup.sh
@@ -89,6 +89,7 @@ sudo pacman -S --noconfirm --needed gparted
 sudo pacman -S --noconfirm --needed simplescreenrecorder
 sudo pacman -S --noconfirm --needed filezilla
 #sudo pacman -S --noconfirm --needed atom
+sudo pacman -S --noconfirm --needed ipset
 sudo pacman -S --noconfirm --needed geany
 sudo pacman -S --noconfirm --needed meld
 sudo pacman -S --noconfirm --needed catfish
