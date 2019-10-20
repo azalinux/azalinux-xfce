c842fc68d0b9408a8b64136bebdc7b231d26a998
azalinux <52433182+azalinux@users.noreply.github.com>
2019-10-20 10:42:45 +1000

    Update xfce_setup.sh

--- xfce_setup.sh
+++ xfce_setup.sh
@@ -105,6 +105,7 @@ sudo pacman -S --noconfirm --needed breeze-icons
 sudo pacman -S --noconfirm --needed elementary-icon-theme
 sudo pacman -S --noconfirm --needed adapta-gtk-theme
 sudo pacman -S --noconfirm --needed network-manager-applet
+sudo pacman -S --noconfirm --needed p7zip
 
 
 #sh sddm.sh
