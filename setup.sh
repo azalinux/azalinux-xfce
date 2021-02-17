0a2fd43343a2da9f3c7d29093029441a234019e4
Azalinux <52433182+azalinux@users.noreply.github.com>
2021-02-17 10:24:03 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,6 +38,14 @@ pacman -S --noconfirm --needed grub
 pacman -S --noconfirm --needed dosfstools
 pacman -S --noconfirm --needed efibootmgr
 
+## OR EFI #####
+#pacman -S efibootmgr
+#!!!! FOR UEFI create /efi partition - FAT32 - 512Mb
+#mkfs.fat -F32 /dev/sdb1
+#!! You have to mount the EFI partition then create the 'efi' directory on it!
+#mkdir /efi
+#mount /dev/sda1 /efi
+#grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB  #!!!!#
 
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
